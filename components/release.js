a655d77957c26f420769e6b476df70f3a379fa92
Justin <justin@campbells.cc>
2018-03-13 19:17:15 +0000

    tuesday commit

---
+++ components/release.js
@@ -0,0 +1,19 @@
+module.exports.releaseRender = () => { 
+  
+        biza: businessunits.biza,
+        bizb: businessunits.bizb,
+        bizc: businessunits.bizc,
+        bizd: businessunits.bizd,
+        bize: businessunits.bize,
+        bizf: businessunits.bizf,
+        failureTech: counters.failureTech,
+        myReleaseScore: counters.myReleaseScore,
+        rteama: releaseTeams.rteama,
+        rteamb: releaseTeams.rteamb,
+        rteamc: releaseTeams.rteamc,
+        rteamd: releaseTeams.rteamd,
+        rteame: releaseTeams.rteame,
+        rteamf: releaseTeams.rteamf
+        
+     
+};
--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@
 
 // load required custom modules
 var calc = require('../components/calc.js');
+// var release = require('../components/release.js');
 
 // load required npm modules
 var express = require('express');
@@ -12,9 +13,37 @@ var app = express();
 // object declare 
 
 var site = {
-  "site":"Release Advisor",
-  "www":"www.rel.com"
+  "site":"Release Robo Advisor",
 }
+
+var releaseTeams = { 
+  rteama: "Target",
+  rteamb: "Lion",
+  rteamc: "Tiger",
+  rteamd: "Shark",
+  rteame: "Batman",
+  rteamf: "Band"
+}
+
+var businessunits = { 
+  "biza": "Commerical Investment Bank",
+  "bizb": "Asset Management",
+  "bizc": "Group I.T",
+  "bizd": "Private Wealth Management",
+  "bize": "Retail Bank"
+}
+
+var counters = {
+  failureTech: 3,
+  myReleaseScore: 4
+}
+
+// setup static content
+app.use('/static', express.static('./static'));
+
+// register partials location
+hbs.registerPartials('./views/partials');
+
 // setup template engine object 
 app.set('view engine', 'hbs');
 
@@ -30,17 +59,48 @@ app.use(express.static('../static'));
  
 // app router
 app.get('/', (req, res) => {
-    res.render('../views/home.hbs', {
-        performCalc: calc.add(2,2,0,10),
-        navTitle: site.site,
-        www: site.www
-    });
+  res.render('../views/home.hbs', {
+  navTitle: site.site,
+  failureTech: counters.failureTech,
+  myReleaseScore: counters.myReleaseScore
+  });
 });
 
 app.get('/release', (req, res) => {
-  res.send('testxxx')
+  res.render('../views/release.hbs', {
+  navTitle: site.site, 
+  biza: businessunits.biza,
+  bizb: businessunits.bizb,
+  bizc: businessunits.bizc,
+  bizd: businessunits.bizd,
+  bize: businessunits.bize,
+  bizf: businessunits.bizf,
+  failureTech: counters.failureTech,
+  myReleaseScore: counters.myReleaseScore,
+  rteama: releaseTeams.rteama,
+  rteamb: releaseTeams.rteamb,
+  rteamc: releaseTeams.rteamc,
+  rteamd: releaseTeams.rteamd,
+  rteame: releaseTeams.rteame,
+  rteamf: releaseTeams.rteamf 
   });
+});
 
+app.get('/releasetechnology', (req, res) => {
+  res.send('releasetechnology');
+});
+
+app.get('/releasescores', (req, res) => {
+  res.send('release scores called');
+});
+
+app.get('/about', (req, res) => {
+  res.send('about called');
+});
+
+app.get('/askrelease', (req, res) => {
+  res.send('api called');
+});
 // setup server
 var server = app.listen(8081, function() {
     var host = server.address().address
